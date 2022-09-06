984e50f34607a33353acee1632214e866ef6720a
Joyson J <72747957+Joyson1819@users.noreply.github.com>
2022-09-06 09:43:41 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,46 @@
+						
+						
+						<form role="form" class="form-horizontal" method= "POST" action="">
+							<div class="form-group">
+								<div class="col-sm-12">
+								<label><b>Why select our College?</b></label>
+								<select  class="form-control">
+									<option>Environment</option>
+									<option>Acadmic</option>
+									<option>Placement</option>
+									<option>Better Knowledge</option>
+									<option>Communication</option>
+								</select>
+								</div>
+							</div>
+							<div class="form-group">
+							<div class="col-sm-12">
+								<label><b>Which department you select?</b></label>
+								<select  class="form-control">
+									<option>CSE</option>
+									<option>ECE</option>
+									<option>EEE</option>
+									<option>IT</option>
+									<option>Mech</option>
+								</select>
+								</div>
+							</div>
+							<div class="form-group">
+							<div class="col-sm-12">
+								<label><b>How to know our college?</b></label>
+								<select  class="form-control">
+									<option>Alumni</option>
+									<option>Students</option>
+									<option>Friends / Relatives</option>
+									<option>Website</option>
+									<option>Advertisement</option>
+								</select>
+								<input type="text" class="form-control" placeholder="Refered By / Contact Person"  />
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-10">
+									<button type="submit" name="btnsubmit" class="btn btn-light btn-radius btn-brd grd1">Continue >></button>
+								</div>
+							</div>
+						</form>
